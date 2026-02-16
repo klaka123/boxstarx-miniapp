0f8b4f0a45e270eb94fd31e4ac81392b11fc63bc
klaka123 <157909481+klaka123@users.noreply.github.com>
2026-02-16 23:40:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,3 +13,16 @@ async def send_gift_menu(bot, user_id, amount):
         f"✨ Чтобы пополнить баланс, подари {amount} ⭐ боту",
         reply_markup=keyboard
     )
+    @dp.message()
+async def webapp_handler(message: types.Message):
+    if message.web_app_data:
+        data = json.loads(message.web_app_data.data)
+
+        if data["action"] == "gift_stars":
+            amount = int(data["amount"])
+
+            await send_gift_menu(
+                bot,
+                message.from_user.id,
+                amount
+            )
